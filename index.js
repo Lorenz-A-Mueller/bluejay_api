3766edb0cea73aaacb2616779fc6648990e91c05
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-10-20 16:17:39 +0200

    Add configuration for heroku to index.js

--- index.js
+++ index.js
@@ -4,32 +4,31 @@ const { ApolloServer, gql } = require('apollo-server');
 
 require('dotenv').config();
 const postgres = require('postgres');
-const sql = postgres();
+// const sql = postgres();
 
 // Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
-// function connectOneTimeToDatabase() {
-//   let sql;
+function connectOneTimeToDatabase() {
+  let sql;
 
-//   if (process.env.NODE_ENV === 'production') {
-//     // Heroku needs SSL connections but
-//     // has an "unauthorized" certificate
-//     // https://devcenter.heroku.com/changelog-items/852
-//     sql = postgres({ ssl: { rejectUnauthorized: false } });
-//   } else {
-//     // When we're in development, make sure that we connect only
-//     // once to the database
-//     if (!globalThis.postgresSqlClient) {
-//       globalThis.postgresSqlClient = postgres();
-//     }
-//     sql = globalThis.postgresSqlClient;
-//   }
+  if (process.env.NODE_ENV === 'production') {
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    // When we're in development, make sure that we connect only
+    // once to the database
+    if (!globalThis.postgresSqlClient) {
+      globalThis.postgresSqlClient = postgres();
+    }
+    sql = globalThis.postgresSqlClient;
+  }
 
-//   return sql;
-// }
+  return sql;
+}
 
-// Connect to PostgreSQL
-// const sql = connectOneTimeToDatabase();
+const sql = connectOneTimeToDatabase();
 
 async function getCustomers() {
   const customers = await sql`
