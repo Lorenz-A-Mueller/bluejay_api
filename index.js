5eedd9a0da076793df3c8bb67efd081e60089b78
Lorenz-A-Mueller <lorenz.a.mueller@gmail.com>
2021-10-30 12:04:59 +0200

    Transform into Apollo-Server-Express that can set cookies

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require('apollo-server-express');
 const { AuthenticationError, UserInputError } = require('apollo-server-errors');
 const setPostgresDefaultsOnHeroku = require('./setPostgresDefaultsOnHeroku.js');
 const { hashPassword, verifyPassword } = require('./utils/auth.js');
 const cookie = require('cookie');
 const crypto = require('node:crypto');
+const cors = require('cors');
+const express = require('express');
 
 require('dotenv').config();
 const postgres = require('postgres');
@@ -51,7 +53,6 @@ async function getCustomerById(id) {
   number, first_name, last_name, email, phone_number, dob, status
   FROM customers
   WHERE id=${id};
-
   `;
   return customer[0];
 }
@@ -60,7 +61,6 @@ async function getCustomerByNumberWithHashedPassword(number) {
   const customer = await sql`
   SELECT * FROM customers
   WHERE number=${number};
-
   `;
   return customer[0];
 }
@@ -92,7 +92,6 @@ async function getEmployeeById(id) {
   number, first_name, last_name, email, password_hashed, dob, admin
   FROM employees
   WHERE id=${id};
-
   `;
   return employee[0];
 }
@@ -101,7 +100,6 @@ async function getEmployeeByNumberWithHashedPassword(number) {
   const employee = await sql`
   SELECT * FROM employees
   WHERE number=${number};
-
   `;
   return employee[0];
 }
@@ -125,7 +123,6 @@ const typeDefs = gql`
     id: ID
     number: [String]
   }
-
   type Query {
     customers: [Customer]
     customer(search: customerSearch!): Customer
@@ -226,7 +223,11 @@ const resolvers = {
         // return;
       }
     },
-    employees: () => {
+    employees: (arg, parent, context) => {
+      console.log('context: ', context);
+      context.res.cookie('abc', 'abc', {
+        httpOnly: true,
+      });
       return getEmployees();
     },
     employee: (parent, args) => {
@@ -244,12 +245,386 @@ const resolvers = {
   },
 };
 
+const app = express();
+const corsOptions = {
+  origin: 'http://localhost:19006',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
   playground: true,
+  context: ({ res }) => ({
+    res,
+  }),
 });
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+
+const main = async () => {
+  await server.start();
+
+  server.applyMiddleware({ app, cors: false, path: '/graphql' });
+
+  // app.get('/rest', (req, res) => {
+  //   res.json({
+  //     data: 'API is working...',
+  //   });
+  // });
+
+  app.listen(process.env.PORT || 4000, () => {
+    console.log(`🚀  Server ready!`);
+  });
+};
+
+main();
+
+// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+//   console.log(`🚀  Server ready at ${url}`);
+// });
+
+// **** APOLLO EXPRESS
+
+// const { ApolloServer, gql } = require('apollo-server-express');
+// const { ApolloServer, gql } = require('apollo-server');
+// const express = require('express');
+// const cors = require('cors');
+// const { AuthenticationError, UserInputError } = require('apollo-server-errors');
+// const setPostgresDefaultsOnHeroku = require('./setPostgresDefaultsOnHeroku.js');
+// const { hashPassword, verifyPassword } = require('./utils/auth.js');
+// const cookie = require('cookie');
+// const crypto = require('node:crypto');
+
+// require('dotenv').config();
+// const postgres = require('postgres');
+
+// setPostgresDefaultsOnHeroku();
+
+// // const sql = postgres();
+
+// // Connect only once to the database
+// // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+// function connectOneTimeToDatabase() {
+//   let sql;
+
+//   if (process.env.NODE_ENV === 'production') {
+//     // Heroku needs SSL connections but
+//     // has an "unauthorized" certificate
+//     // https://devcenter.heroku.com/changelog-items/852
+//     sql = postgres({ ssl: { rejectUnauthorized: false } });
+//   } else {
+//     // When we're in development, make sure that we connect only
+//     // once to the database
+//     if (!globalThis.postgresSqlClient) {
+//       globalThis.postgresSqlClient = postgres();
+//     }
+//     sql = globalThis.postgresSqlClient;
+//   }
+
+//   return sql;
+// }
+
+// const sql = connectOneTimeToDatabase();
+
+// async function getCustomers() {
+//   const customers = await sql`
+//   SELECT
+//   number, first_name, last_name, email, password_hashed, phone_number, dob, status
+//   FROM customers;
+//   `;
+//   return customers;
+// }
+
+// async function getCustomerById(id) {
+//   const customer = await sql`
+//   SELECT
+//   number, first_name, last_name, email, phone_number, dob, status
+//   FROM customers
+//   WHERE id=${id};
+
+//   `;
+//   return customer[0];
+// }
+
+// async function getCustomerByNumberWithHashedPassword(number) {
+//   const customer = await sql`
+//   SELECT * FROM customers
+//   WHERE number=${number};
+
+//   `;
+//   return customer[0];
+// }
+
+// async function createCustomer(newCustomer) {
+//   const customer = await sql`
+//   INSERT INTO customers
+//   (first_name, last_name, email, password, phone_number, dob, status)
+//   VALUES
+//   (${newCustomer.first_name}, ${newCustomer.last_name}, ${newCustomer.email}, ${newCustomer.password}, ${newCustomer.phone_number}, ${newCustomer.dob}, ${newCustomer.status})
+//   RETURNING
+//   (first_name, last_name, email, phone_number, dob, status)
+//   `;
+//   return customer[0];
+// }
+
+// async function getEmployees() {
+//   const employees = await sql`
+//   SELECT
+//   number, first_name, last_name, email, dob, admin
+//   FROM employees;
+//   `;
+//   return employees;
+// }
+
+// async function getEmployeeById(id) {
+//   const employee = await sql`
+//   SELECT
+//   number, first_name, last_name, email, password_hashed, dob, admin
+//   FROM employees
+//   WHERE id=${id};
+
+//   `;
+//   return employee[0];
+// }
+
+// async function getEmployeeByNumberWithHashedPassword(number) {
+//   const employee = await sql`
+//   SELECT * FROM employees
+//   WHERE number=${number};
+
+//   `;
+//   return employee[0];
+// }
+
+// async function createCustomerSession(token, id) {
+//   const session = await sql`
+//   INSERT INTO customer_sessions
+//   (token, customer_id)
+//   VALUES
+//   (${token}, ${id})
+//   RETURNING
+//   *
+//   `;
+// }
+
+// async function deleteExpiredSessions() {
+//   const sessions = await sql`
+//   DELETE FROM customer_sessions
+//   WHERE expiry_timestamp < NOW()
+//   RETURNING *
+
+//   `;
+// }
+
+// const typeDefs = gql`
+//   scalar Date
+//   input customerSearch {
+//     id: ID
+//     number: [String]
+//   }
+//   input employeeSearch {
+//     id: ID
+//     number: [String]
+//   }
+
+//   type Query {
+//     customers: [Customer]
+//     customer(search: customerSearch!): Customer
+//     employees: [Employee]
+//     employee(search: employeeSearch!): Employee
+//   }
+//   type Mutation {
+//     createCustomer(
+//       number: String!
+//       first_name: String!
+//       last_name: String!
+//       email: String!
+//       password_hashed: String!
+//       phone_number: String!
+//       dob: Date!
+//       status: String!
+//     ): Customer
+//   }
+//   type Customer {
+//     id: ID
+//     number: String
+//     first_name: String
+//     last_name: String
+//     email: String
+//     password_hashed: String
+//     phone_number: String
+//     dob: Date
+//     status: String
+//   }
+//   # type CustomerWithoutPassword {
+//   #   id: ID
+//   #   number: String
+//   #   first_name: String
+//   #   last_name: String
+//   #   email: String
+//   #   phone_number: String
+//   #   dob: Date
+//   #   status: String
+//   # }
+//   type Employee {
+//     id: ID
+//     number: String
+//     first_name: String
+//     last_name: String
+//     email: String
+//     password_hashed: String
+//     dob: Date
+//     admin: Boolean
+//   }
+//   # type EmployeeWithoutPassword {
+//   #   id: ID
+//   #   number: String
+//   #   first_name: String
+//   #   last_name: String
+//   #   email: String
+//   #   dob: Date
+//   #   admin: Boolean
+//   # }
+// `;
+
+// const resolvers = {
+//   Query: {
+//     customers: () => {
+//       return getCustomers();
+//     },
+//     customer: async (parent, args, context) => {
+//       if (args.search.id) return getCustomerById(args.search.id);
+//       if (args.search.number) {
+//         console.log('first arg: ', args.search.number[0]);
+//         console.log('second arg: ', args.search.number[1]);
+
+//         if (!args.search.number[0] || !args.search.number[1]) {
+//           throw new UserInputError('Password and Username are required!');
+//         }
+
+//         const hashedPasswordInDb = await getCustomerByNumberWithHashedPassword(
+//           args.search.number[0],
+//         );
+//         // console.log(
+//         //   'hashed password in db: ',
+//         //   hashedPasswordInDb.password_hashed,
+//         // );
+//         const passWordsMatch = await verifyPassword(
+//           args.search.number[1],
+//           hashedPasswordInDb.password_hashed,
+//         );
+//         console.log('passwordsmatch', passWordsMatch);
+//         if (passWordsMatch) {
+//           const { password_hashed, ...customerWithoutHashedPassword } =
+//             await getCustomerByNumberWithHashedPassword(args.search.number[0]);
+
+//           // delete all expired sessions (even other peoples'!)
+
+//           deleteExpiredSessions();
+//           console.log('context', context);
+
+//           // create token
+//           const token = crypto.randomBytes(64).toString('base64');
+//           // do db query to add session
+//           createCustomerSession(token, customerWithoutHashedPassword.id);
+//           // set the Cookie in the requester's browser
+
+//           // const maxAge = 60 * 5; // 5 Min.
+//           // const isProduction = process.env.NODE_ENV === 'production';
+//           // const thisCookie = cookie.serialize('sessionToken', token, {
+//           //   maxAge: 300,
+//           //   expires: new Date(Date.now() + maxAge * 1000),
+//           //   httpOnly: true,
+//           //   secure: isProduction,
+//           //   path: '/',
+//           //   sameSite: 'lax',
+//           // });
+//           // console.log('thisCookie: ', thisCookie);
+//           // console.log('response: ', Response);
+
+//           // ****
+//           // context.setCookies.push({
+//           //   domain: 'http://localhost:19006',
+//           //   name: 'cookieName',
+//           //   value: 'cookieContent',
+//           //   options: {
+//           //     // expires: new Date('2022-12-31T00:00:00'),
+//           //     httpOnly: true,
+//           //     maxAge: 3600,
+//           //     path: '/',
+//           //     sameSite: 'lax',
+//           //     secure: true,
+//           //   },
+//           // });
+//           // *****
+//           console.log('context: ', context);
+//           console.log('context.res: ', context.res);
+//           console.log('context.res.cookie: ', context.res.cookie);
+
+//           return 'HI';
+
+//           // return customerWithoutHashedPassword;
+//         }
+//         // if (!passWordsMatch) {
+//         throw new AuthenticationError(
+//           'Password/Username combination did not match!',
+//         );
+//         // }
+//         // return;
+//       }
+//     },
+//     employees: () => {
+//       return getEmployees();
+//     },
+//     employee: (parent, args) => {
+//       if (args.search.id) return getEmployeeById(args.search.id);
+//       if (args.search.number) {
+//         return getEmployeeByNumberWithHashedPassword(args.search.number[0]);
+//       }
+//     },
+//   },
+//   Mutation: {
+//     createCustomer: (parent, args) => {
+//       args.password = hashPassword(args.password);
+//       return createCustomer(args);
+//     },
+//   },
+// };
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   introspection: true,
+//   playground: true,
+//   context: ({ res }) => ({
+//     res,
+//   }),
+// });
+// // context: {
+
+// // },
+
+// const main = async () => {
+//   const app = express();
+//   const corsOptions = {
+//     origin: '*',
+//     credentials: true,
+//   };
+//   app.use(cors(corsOptions));
+//   await server.start();
+
+//   server.applyMiddleware({ app, cors: false, path: '/graphql' });
+
+//   app.get('/rest', (req, res) => {
+//     res.json({
+//       data: 'API is working...',
+//     });
+//   });
+
+//   app.listen(process.env.PORT || 4000, () => {
+//     console.log(`🚀  Server ready!`);
+//   });
+// };
+
+// main();
